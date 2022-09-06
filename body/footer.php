aeeb72a6850a5050d9493428fdeb4e143bed4363
mehmetakgul <mehmetakgul.dev@gmail.com>
2022-09-06 22:44:12 +0300

    fix

--- body/footer.php
+++ body/footer.php
@@ -12,7 +12,6 @@
                 <li><a href="../a-b-testing.php">A/B Testing</a></li>
                 <li><a href="../add-remove-elements.php">Add/Remove Elements</a></li>
                 <li><a href="../autocomplete.php">Autocomplete</a></li>
-                <li><a href="../basic-auth.php">Basic Auth (user and pass: admin)</a></li>
                 <li><a href="../broken-images.php">Broken Images</a></li>
                 <li><a href="../challenging-dom.php">Challenging DOM</a></li>
                 <li><a href="../checkboxes.php">Checkboxes</a></li>
@@ -30,7 +29,6 @@
                 <li><a href="../file-upload.php">File Upload</a></li>
                 <li><a href="../forgot-password.php">Forgot Password</a></li>
                 <li><a href="../form-authentication.php">Form Authentication</a></li>
-                <li><a href="../frames.php">Frames</a></li>
                 <li><a href="../geolocation.php">Geolocation</a></li>
                 <li><a href="../floating-menu.php">Floating Menu</a></li>
                 <li><a href="../horizontal-slider.php">Horizontal Slider</a></li>
@@ -41,23 +39,16 @@
                 <li><a href="../javascript-alerts.php">JavaScript Alerts</a></li>
                 <li><a href="../javascript-onload-event-error.php">JavaScript onload event error</a></li>
                 <li><a href="../key-presses.php">Key Presses</a></li>
-                <li><a href="../large-deep-dom.php">Large & Deep DOM</a></li>
                 <li><a href="../multiple-buttons.php">Multiple Buttons</a></li>
                 <li><a href="../multiple-windows.php">Multiple Windows</a></li>
-                <li><a href="../nested-frames.php">Nested Frames</a></li>
                 <li><a href="../new-tab.php">New tab</a></li>
                 <li><a href="../notification-messages.php">Notification Messages</a></li>
                 <li><a href="../radio-buttons.php">Radio Buttons</a></li>
-                <li><a href="../redirect-link.php">Redirect Link</a></li>
                 <li><a href="../registration-form.php">Registration Form</a></li>
                 <li><a href="../secure-file-download.php">Secure File Download</a></li>
-                <li><a href="../shifting-content.php">Shifting Content</a></li>
-                <li><a href="../sign-up-for-mailing-list.php">Sign Up For Mailing List</a></li>
-                <li><a href="../slow-resources.php">Slow Resources</a></li>
                 <li><a href="../sortable-data-tables.php">Sortable Data Tables</a></li>
                 <li><a href="../status-codes.php">Status Codes</a></li>
-                 <li><a href="../wysiwyg-ditor.php">WYSIWYG Editor</a></li>
-                <li><a href="../web-tables.php">Web Tables</a></li>
+                <li><a href="../wysiwyg-ditor.php">WYSIWYG Editor</a></li>
             </ol>
         </div>
 
--- pages/home.php
+++ pages/home.php
@@ -1,72 +1,63 @@
-
 <?php require_once("body/header.php"); ?>
-            <!-- col-md-8 start-->
-            <article class="blog-post">
-                <h2 class="blog-post-title mb-1">Summary</h2>
-                <p class="blog-post-meta">September 1, 2022 by <a href="TechProEducation.com">TechProEducation.com</a>
-                </p>
-                <h3>Applications lists</h3>
-                <p>This website has been prepared for Automation Testers and has been prepared to exemplify all the
-                    applications in the industry.</p>
-                <hr>
-                <div class="row col-md-12">
-                    <div class="col-md-6">
-                        <ul>
-                            <li><a href="a-b-testing.php">A/B Testing</a></li>
-                            <li><a href="add-remove-elements.php">Add/Remove Elements</a></li>
-                            <li><a href="autocomplete.php">Autocomplete</a></li>
-                            <li><a href="basic-auth.php">Basic Auth (user and pass: admin)</a></li>
-                            <li><a href="broken-images.php">Broken Images</a></li>
-                            <li><a href="challenging-dom.php">Challenging DOM</a></li>
-                            <li><a href="checkboxes.php">Checkboxes</a></li>
-                            <li><a href="context-menu.php">Context Menu</a></li>
-                            <li><a href="disappearing-elements.php">Disappearing Elements</a></li>
-                            <li><a href="drag-and-drop.php">Drag and Drop</a></li>
-                            <li><a href="drag-and-drop-circles.php">Drag and Drop Circles</a></li>
-                            <li><a href="dropdown.php">Dropdown</a></li>
-                            <li><a href="dynamic-content.php">Dynamic Content</a></li>
-                            <li><a href="dynamic-controls.php">Dynamic Controls</a></li>
-                            <li><a href="drag-and-drop.php">Dynamic Loading</a></li>
-                            <li><a href="entry-ad.php">Entry Ad</a></li>
-                            <li><a href="exit-intent.php">Exit Intent</a></li>
-                            <li><a href="file-download.php">File Download</a></li>
-                            <li><a href="file-upload.php">File Upload</a></li>
-                            <li><a href="floating-menu.php">Floating Menu</a></li>
-                            <li><a href="form-authentication.php">Form Authentication</a></li>
-                             <li><a href="geolocation.php">Geolocation</a></li>
-                            <li><a href="floating-menu.php">Floating Menu</a></li>
-                            <li><a href="horizontal-slider.php">Horizontal Slider</a></li>
-                            <li><a href="hovers.php">Hovers</a></li>
-                            <li><a href="infinite-scroll.php">Infinite Scroll</a></li>
-                            <li><a href="inputs.php">Inputs</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-md-6">
-                        <ul>
-                            <li><a href="jquery-ui-menus.php">JQuery UI Menus</a></li>
-                            <li><a href="javascript-alerts.php">JavaScript Alerts</a></li>
-                            <li><a href="javascript-onload-event-error.php">JavaScript onload event error</a></li>
-                            <li><a href="key-presses.php">Key Presses</a></li>
-                            <li><a href="large-deep-dom.php">Large & Deep DOM</a></li>
-                            <li><a href="multiple-buttons.php">Multiple Buttons</a></li>
-                            <li><a href="multiple-windows.php">Multiple Windows</a></li>
-                            <li><a href="nested-frames.php">Nested Frames</a></li>
-                            <li><a href="new-tab.php">New tab</a></li>
-                            <li><a href="notification-messages.php">Notification Messages</a></li>
-                            <li><a href="radio-buttons.php">Radio Buttons</a></li>
-                            <li><a href="redirect-link.php">Redirect Link</a></li>
-                            <li><a href="secure-file-download.php">Secure File Download</a></li>
-                            <li><a href="shifting-content.php">Shifting Content</a></li>
-                            <li><a href="sign-up-for-mailing-list.php">Sign Up For Mailing List</a></li>
-                            <li><a href="slow-resources.php">Slow Resources</a></li>
-                            <li><a href="sortable-data-tables.php">Sortable Data Tables</a></li>
-                            <li><a href="status-codes.php">Status Codes</a></li>
-                            <li><a href="typos.php">Typos</a></li>
-                            <li><a href="wysiwyg-ditor.php">WYSIWYG Editor</a></li>
-                            <li><a href="web-tables.php">Web Tables</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </article>
-            <!-- col-md-8 finis-->
+<!-- col-md-8 start-->
+<article class="blog-post">
+    <h2 class="blog-post-title mb-1">Summary</h2>
+    <p class="blog-post-meta">September 1, 2022 by <a href="TechProEducation.com">TechProEducation.com</a>
+    </p>
+    <h3>Applications lists</h3>
+    <p>This website has been prepared for Automation Testers and has been prepared to exemplify all the
+        applications in the industry.</p>
+    <hr>
+    <div class="row col-md-12">
+        <div class="col-md-6">
+            <ul>
+                <li><a href="a-b-testing.php">A/B Testing</a></li>
+                <li><a href="add-remove-elements.php">Add/Remove Elements</a></li>
+                <li><a href="autocomplete.php">Autocomplete</a></li>
+                <li><a href="broken-images.php">Broken Images</a></li>
+                <li><a href="challenging-dom.php">Challenging DOM</a></li>
+                <li><a href="checkboxes.php">Checkboxes</a></li>
+                <li><a href="context-menu.php">Context Menu</a></li>
+                <li><a href="disappearing-elements.php">Disappearing Elements</a></li>
+                <li><a href="drag-and-drop.php">Drag and Drop</a></li>
+                <li><a href="drag-and-drop-circles.php">Drag and Drop Circles</a></li>
+                <li><a href="dropdown.php">Dropdown</a></li>
+                <li><a href="dynamic-content.php">Dynamic Content</a></li>
+                <li><a href="dynamic-controls.php">Dynamic Controls</a></li>
+                <li><a href="drag-and-drop.php">Dynamic Loading</a></li>
+                <li><a href="entry-ad.php">Entry Ad</a></li>
+                <li><a href="exit-intent.php">Exit Intent</a></li>
+                <li><a href="file-download.php">File Download</a></li>
+                <li><a href="file-upload.php">File Upload</a></li>
+                <li><a href="floating-menu.php">Floating Menu</a></li>
+                <li><a href="form-authentication.php">Form Authentication</a></li>
+
+            </ul>
+        </div>
+        <div class="col-md-6">
+            <ul>
+                <li><a href="geolocation.php">Geolocation</a></li>
+                <li><a href="floating-menu.php">Floating Menu</a></li>
+                <li><a href="horizontal-slider.php">Horizontal Slider</a></li>
+                <li><a href="hovers.php">Hovers</a></li>
+                <li><a href="infinite-scroll.php">Infinite Scroll</a></li>
+                <li><a href="inputs.php">Inputs</a></li>
+                <li><a href="jquery-ui-menus.php">JQuery UI Menus</a></li>
+                <li><a href="javascript-alerts.php">JavaScript Alerts</a></li>
+                <li><a href="javascript-onload-event-error.php">JavaScript onload event error</a></li>
+                <li><a href="key-presses.php">Key Presses</a></li>
+                <li><a href="multiple-buttons.php">Multiple Buttons</a></li>
+                <li><a href="multiple-windows.php">Multiple Windows</a></li>
+                <li><a href="new-tab.php">New tab</a></li>
+                <li><a href="notification-messages.php">Notification Messages</a></li>
+                <li><a href="radio-buttons.php">Radio Buttons</a></li>
+                <li><a href="secure-file-download.php">Secure File Download</a></li>
+                <li><a href="sortable-data-tables.php">Sortable Data Tables</a></li>
+                <li><a href="status-codes.php">Status Codes</a></li>
+                <li><a href="wysiwyg-ditor.php">WYSIWYG Editor</a></li>
+            </ul>
+        </div>
+    </div>
+</article>
+<!-- col-md-8 finis-->
 <?php require_once("body/footer.php"); ?>
